093a51442c761d0cad17a1a8cd28c18ac00ae868
AverageBadCoder <jmadover@gmail.com>
2024-11-07 10:07:50 -0500

    ashdsahd

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iterativeTest.java
@@ -0,0 +1,309 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This file contains an example of a Linear "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode is executed.
+ *
+ * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ *
+ * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ *
+ * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+ * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+ * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+ *
+ * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
+ * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
+ * the direction of all 4 motors (see code below).
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Driver Control", group="Linear OpMode")
+
+public class iterativeTest extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor lf = null;
+    private DcMotor lb = null;
+    private DcMotor rf = null;
+    private DcMotor rb = null;
+    private DcMotor lift = null;
+    private DcMotor reach = null;
+    // dwayne the double reverse four bar
+    //private DcMotor dave = null;
+    private CRServo intake1 = null;
+    private CRServo intake2 = null;
+    private Servo wrist1 = null;
+    private Servo wrist2 = null;
+    private Servo shoulder1 = null;
+    private Servo shoulder2 = null;
+    IMU imu;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+        lf = hardwareMap.get(DcMotor.class, "leftFront");
+        lb = hardwareMap.get(DcMotor.class, "leftBack");
+        rf = hardwareMap.get(DcMotor.class, "rightFront");
+        rb = hardwareMap.get(DcMotor.class, "rightBack");
+        imu = hardwareMap.get(IMU.class, "imu");
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        reach = hardwareMap.get(DcMotor.class, "reach");
+        //dave = hardwareMap.get(DcMotor.class, "dave");
+        intake1 = hardwareMap.get(CRServo.class, "intake1");
+        intake2 = hardwareMap.get(CRServo.class, "intake2");
+        wrist1 = hardwareMap.get(Servo.class, "wrist1");
+        wrist2 = hardwareMap.get(Servo.class, "wrist2");
+        shoulder1 = hardwareMap.get(Servo.class, "shoulder1");
+        shoulder2 = hardwareMap.get(Servo.class, "shoulder2");
+
+
+        // ########################################################################################
+        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
+        // ########################################################################################
+        // Most robots need the motors on one side to be reversed to drive forward.
+        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
+        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
+        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
+        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
+        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
+        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        lb.setDirection(DcMotor.Direction.REVERSE);
+        rf.setDirection(DcMotor.Direction.FORWARD);
+        rb.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotor.Direction.FORWARD);
+        reach.setDirection(DcMotor.Direction.FORWARD);
+        //dave.setDirection(DcMotor.Direction.FORWARD);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        reach.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //dave.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+//        //TODO hub orientation
+//        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+//        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.LEFT;
+//
+//        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+//        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double max;
+
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+//            YawPitchRollAngles robotOrientation;
+//            robotOrientation = imu.getRobotYawPitchRollAngles();
+//            double robotyaw = robotOrientation.getYaw(AngleUnit.DEGREES);
+//            double axial = oglateral*Math.cos(robotyaw)-ogaxial*Math.sin(robotyaw);
+//            double lateral = oglateral*Math.cos(robotyaw)+ogaxial*Math.sin(robotyaw);
+
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower  = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower   = axial - lateral + yaw;
+            double rightBackPower  = axial + lateral - yaw;
+
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower  /= max;
+                rightFrontPower /= max;
+                leftBackPower   /= max;
+                rightBackPower  /= max;
+            }
+
+            // This is test code:
+            //
+            // Uncomment the following code to test your motor directions.
+            // Each button should make the corresponding motor run FORWARD.
+            //   1) First get all the motors to take to correct positions on the robot
+            //      by adjusting your Robot Configuration if necessary.
+            //   2) Then make sure they run in the correct direction by modifying the
+            //      the setDirection() calls above.
+            // Once the correct motors move in the correct direction re-comment this code.
+
+            /*
+            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
+            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
+            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
+            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
+            */
+
+            // Send calculated power to wheels
+
+            if(gamepad1.dpad_up){
+                leftFrontPower  = 0.1;
+                rightFrontPower = 0.1;
+                leftBackPower   = 0.1;
+                rightBackPower  = 0.1;
+            }
+            if(gamepad1.dpad_down){
+                leftFrontPower  = -0.1;
+                rightFrontPower = -0.1;
+                leftBackPower   = -0.1;
+                rightBackPower  = -0.1;
+            }
+            if(gamepad1.dpad_left){
+                leftFrontPower  = -0.2;
+                rightFrontPower = 0.2;
+                leftBackPower   = 0.2;
+                rightBackPower  = -0.2;
+            }
+            if(gamepad1.dpad_right){
+                leftFrontPower  = 0.2;
+                rightFrontPower = -0.2;
+                leftBackPower   = -0.2;
+                rightBackPower  = 0.2;
+            }
+            if(gamepad1.right_bumper){
+                leftFrontPower  = 0.2;
+                rightFrontPower = -0.2;
+                leftBackPower   = 0.2;
+                rightBackPower  = -0.2;
+            }
+            if(gamepad1.left_bumper){
+                leftFrontPower  = -0.2;
+                rightFrontPower = 0.2;
+                leftBackPower   = -0.2;
+                rightBackPower  = 0.2;
+            }
+
+
+            lift.setPower(-gamepad2.left_stick_y);
+            reach.setPower(-gamepad2.right_stick_y);
+
+//            if(gamepad2.right_trigger > 0.5){
+//                dave.setPower(-0.6);
+//                //down
+//            }
+//            if(gamepad2.left_trigger > 0.5){
+//                dave.setPower(0.6);
+//                //up
+//            }
+            intake2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            if(gamepad2.a){
+                //open
+                intake1.setPower(0.5);
+                intake2.setPower(0.5);
+            }
+            if(gamepad2.b){
+                //close
+                intake1.setPower(-0.5);
+                intake2.setPower(-0.5);
+            }
+            else{
+                intake1.setPower(0);
+                intake2.setPower(0);
+            }
+
+            //TODO get wrist and shoulder servo position
+            if(gamepad2.right_bumper){
+                //wrist up
+                wrist1.setPosition(1);
+                wrist2.setPosition(1);
+            }
+
+            if(gamepad2.left_bumper){
+                //wrist down
+                wrist1.setPosition(2);
+                wrist2.setPosition(2);
+            }
+
+            if(gamepad2.right_trigger >= 0.5){
+                //shoulder up
+                shoulder1.setPosition(1);
+                shoulder2.setPosition(1);
+            }
+
+            if(gamepad2.left_trigger >= 0.5){
+                //shoudler down
+                shoulder1.setPosition(2);
+                shoulder2.setPosition(2);
+            }
+
+
+            lf.setPower(leftFrontPower);
+            rf.setPower(rightFrontPower);
+            lb.setPower(leftBackPower);
+            rb.setPower(rightBackPower);
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Quantum on top", '#'+ 1);
+            //telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
+            //telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.update();
+        }
+    }}
